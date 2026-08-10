45121489e657978134798509a54acba6c3a00a2a
carnifex38 <petermarkdorosz@hotmail.com>
2026-08-10 19:19:02 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+build: {
+    outDir: 'docs', // Changes the output directory from 'dist' to 'docs'
+    emptyOutDir: true, // Empties the folder on every build
+  },
   plugins: [react()],
 });
