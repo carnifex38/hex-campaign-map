8371339b3636e32eb326f1f0f33f96bb84e1c338
Singulo <jblack144@gmail.com>
2026-08-10 19:26:20 -0400

    Update build output directory in vite.config.js
    Change the output directory for the build from 'docs' to './'.

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
 build: {
-    outDir: 'docs', // Changes the output directory from 'dist' to 'docs'
-    emptyOutDir: true, // Empties the folder on every build
+    outDir: './', // Changes the output directory from 'dist' to 'docs'
   },
   plugins: [react()],
 });
